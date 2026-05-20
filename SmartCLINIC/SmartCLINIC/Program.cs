9f85893a67a8a20b27e12e030a89b5afcc637f51
lalajbegov1 <lalajbegov1@etf.unsa.ba>
2026-05-20 19:26:38 +0200

    fix

--- SmartCLINIC/SmartCLINIC/Program.cs
+++ SmartCLINIC/SmartCLINIC/Program.cs
@@ -11,15 +11,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<Korisnik>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDefaultIdentity<Korisnik>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = false;
-})
-.AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
